d74072f281f9099563133fa3183c478ebc2e3391
FT\thm <thm@forcetechnology.com>
2024-09-03 14:09:51 +0200

    langchain-community setup

---
+++ libs/community/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain-community',
+    version='0.2.15',
+    packages=find_packages(include=['langchain_community']),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
