78cf92033c8333a70f5981e770795e8d64cc3100
FT\thm <thm@forcetechnology.com>
2024-09-03 10:01:54 +0200

    setup.py typo.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='langchain',
     version='0.1',
-    packages= packages=find_packages(include=['libs']),
+    packages=find_packages(include=['libs']),
     install_requires=[
         # Add your dependencies here
     ],
