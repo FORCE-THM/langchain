709abfdab0d9d15901b492c1b16ee568a47545f8
FT\thm <thm@forcetechnology.com>
2024-09-03 14:06:34 +0200

    ...

--- libs/text-splitters/setup.py
+++ libs/text-splitters/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='langchain-text-splitters ',
     version='0.2.2',
-    packages=find_packages(),
+    packages=find_packages(include=['langchain_text_splitters']),
     install_requires=[
         # Add your dependencies here
     ],
