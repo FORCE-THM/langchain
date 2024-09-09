a280db661c629db3ccbd7702854efd820b1a25a6
FT\thm <thm@forcetechnology.com>
2024-09-09 22:25:18 +0200

    langchain-core setup

---
+++ libs/core/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain-core',
+    version='0.2.38',
+    packages=find_packages(include=['langchain_core']),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
